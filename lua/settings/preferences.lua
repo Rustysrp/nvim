401bd1e16d98aa501816649939a0a663c4579a8c
Rustysrp <spunkeyy0@gmail.com>
2022-09-05 13:16:04 -0400

    Undo saves arcoss multiple sessions

--- lua/settings/preferences.lua
+++ lua/settings/preferences.lua
@@ -1,30 +1,32 @@
+-- Local variables
 local o = vim.o
 local A = vim.api
 
--- colors + theme
+-- Colors and theme
 vim.cmd "colorscheme gusher"
 o.termguicolors = true
 o.background = 'dark'
 
--- improve ui
+-- Improve ui
 o.number = true
 o.cursorline = true
 o.relativenumber = true
 
--- better editing
+-- Better editing
 o.expandtab = true
 o.smarttab = true
 o.wrap = true
 o.tabstop = 4
 o.expandtab = true
 o.shiftwidth = 0
+o.ttyfast = true
+o.undodir = "~/.vimdid"
 o.undofile = true
 
--- searches aren't case sensitive unless /C is used
+-- Searches aren't case sensitive unless /C is used
 o.ignorecase = true
 o.smartcase = true
 
--- better buffer splitting
+-- Better buffer splitting
 o.splitbelow = true
 o.splitright = true
-
