2e7c3e572cd83b42e9195c7e5e122d3803903f2f
Rustysrp <spunkeyy0@gmail.com>
2022-09-05 18:06:18 -0400

    Added keybindings for insert mode navigation

--- lua/settings/keybinds.lua
+++ lua/settings/keybinds.lua
@@ -6,7 +6,6 @@ function map(mode, lhs, rhs, opts)
     end
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
-
 -- Set leader keys to space
 vim.g.mapleader = ' '
 
@@ -20,6 +19,9 @@ map("n", "<C-x>", ":tabc<CR>")
 map("n", "H", "gT")
 map("n", "L", "gt")
 
+-- Go to top of file 
+map("n", "<leader>g", "gg")
+
 -- Faster saving and quitting
 map("n", "<leader>w", ":w<CR>")
 map("n", "<leader>q", ":q<CR>")
@@ -29,3 +31,15 @@ map("n", "<leader>r", "<C-r>")
 
 -- Nerdtree keybinds
 map("n", "<leader>n", ":NERDTreeToggle<CR>")
+
+-- Insert mode arrow keybinds
+map("i", "<C-l>", "<right>")
+map("i", "<C-h>", "<left>")
+map("i", "<C-j>", "<down>")
+map("i", "<C-k>", "<up>")
+
+-- Delete line
+map("i", "<C-q>", "<ESC>ddi")
+
+-- Insert line (mimic normal 'o')
+map("i", "<C-o>", "<down><ESC>0i<C-j><up>")
